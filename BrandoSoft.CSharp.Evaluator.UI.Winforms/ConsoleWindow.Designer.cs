8d07b8e3b8ae928c41adef7873abb6088ce94b04
BrandonLegault <bdlegault@gmail.com>
2016-10-03 16:07:50 -0400

    Add the UI files to the project

---
+++ BrandoSoft.CSharp.Evaluator.UI.Winforms/ConsoleWindow.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BrandoSoft.Evaluator.UI.Winforms
+{
+    partial class ConsoleWindow
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (this.components != null))
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConsoleIn = new System.Windows.Forms.TextBox();
+            this.txtConsoleOut = new System.Windows.Forms.TextBox();
+            this.splContainer = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splContainer)).BeginInit();
+            this.splContainer.Panel1.SuspendLayout();
+            this.splContainer.Panel2.SuspendLayout();
+            this.splContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtConsoleIn
+            // 
+            this.txtConsoleIn.BackColor = System.Drawing.Color.Black;
+            this.txtConsoleIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsoleIn.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsoleIn.ForeColor = System.Drawing.Color.White;
+            this.txtConsoleIn.Location = new System.Drawing.Point(0, 0);
+            this.txtConsoleIn.Margin = new System.Windows.Forms.Padding(0);
+            this.txtConsoleIn.Name = "txtConsoleIn";
+            this.txtConsoleIn.Size = new System.Drawing.Size(855, 20);
+            this.txtConsoleIn.TabIndex = 1;
+            this.txtConsoleIn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtConsoleIn_KeyUp);
+            // 
+            // txtConsoleOut
+            // 
+            this.txtConsoleOut.BackColor = System.Drawing.Color.Black;
+            this.txtConsoleOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsoleOut.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsoleOut.ForeColor = System.Drawing.Color.White;
+            this.txtConsoleOut.Location = new System.Drawing.Point(0, 0);
+            this.txtConsoleOut.Margin = new System.Windows.Forms.Padding(0);
+            this.txtConsoleOut.Multiline = true;
+            this.txtConsoleOut.Name = "txtConsoleOut";
+            this.txtConsoleOut.ReadOnly = true;
+            this.txtConsoleOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConsoleOut.Size = new System.Drawing.Size(855, 643);
+            this.txtConsoleOut.TabIndex = 0;
+            // 
+            // splContainer
+            // 
+            this.splContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splContainer.Location = new System.Drawing.Point(0, 0);
+            this.splContainer.Margin = new System.Windows.Forms.Padding(0);
+            this.splContainer.Name = "splContainer";
+            this.splContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splContainer.Panel1
+            // 
+            this.splContainer.Panel1.Controls.Add(this.txtConsoleOut);
+            // 
+            // splContainer.Panel2
+            // 
+            this.splContainer.Panel2.Controls.Add(this.txtConsoleIn);
+            this.splContainer.Panel2MinSize = 0;
+            this.splContainer.Size = new System.Drawing.Size(855, 672);
+            this.splContainer.SplitterDistance = 643;
+            this.splContainer.SplitterWidth = 1;
+            this.splContainer.TabIndex = 2;
+            // 
+            // ConsoleWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splContainer);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "ConsoleWindow";
+            this.Size = new System.Drawing.Size(855, 672);
+            this.Resize += new System.EventHandler(this.ConsoleWindow_Resize);
+            this.splContainer.Panel1.ResumeLayout(false);
+            this.splContainer.Panel1.PerformLayout();
+            this.splContainer.Panel2.ResumeLayout(false);
+            this.splContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splContainer)).EndInit();
+            this.splContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtConsoleOut;
+        private System.Windows.Forms.TextBox txtConsoleIn;
+        private System.Windows.Forms.SplitContainer splContainer;
+    }
+}
---
+++ BrandoSoft.CSharp.Evaluator.UI.Winforms/ConsoleWindow.cs
@@ -0,0 +1,108 @@
+﻿namespace BrandoSoft.Evaluator.UI.Winforms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Exposes the Expression Evaluator as a console window.
+    /// </summary>
+    public partial class ConsoleWindow 
+        : UserControl
+    {
+        #region Variables
+
+        /// <summary>
+        /// A list of our previously entered debug commands
+        /// </summary>
+        private readonly List<string> _previousDebugCommands;
+
+        /// <summary>
+        /// Keeps index so the user can press up or down to retrieve past commands
+        /// </summary>
+        private int _debugCommandIndex;
+        #endregion
+
+
+        #region Constructor
+
+        public ConsoleWindow()
+        {
+            this.InitializeComponent();
+
+            this._previousDebugCommands = new List<string>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the text that appears in the output box of the console window
+        /// </summary>
+        public string OutputText
+        {
+            get { return this.txtConsoleOut.Text; }
+            set { this.txtConsoleOut.Text = value; }
+        }
+
+        /// <summary>
+        /// True if the input textbox of this control should be multiline.
+        /// </summary>
+        public bool MultilineInput { get; set; }
+
+        /// <summary>
+        /// The key (or key combination) that should be used to submit text to the expression evaluator. This is a flags field.
+        /// </summary>
+        public Keys AcceptKey { get; set; }
+
+        #endregion
+
+
+        private void txtConsoleIn_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                this._debugCommandIndex = Math.Max(0, this._debugCommandIndex - 1);
+                this.txtConsoleIn.Text = this._previousDebugCommands[this._debugCommandIndex];
+                this.txtConsoleIn.Select(this.txtConsoleIn.TextLength + 1, 0);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                this._debugCommandIndex = Math.Min(this._previousDebugCommands.Count, this._debugCommandIndex + 1);
+                this.txtConsoleIn.Text = this._debugCommandIndex < this._previousDebugCommands.Count
+                                       ? this._previousDebugCommands[this._debugCommandIndex]
+                                       : string.Empty;
+                this.txtConsoleIn.Select(this.txtConsoleIn.TextLength + 1, 0);
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                //This function checks whether or not we can submit the command before submitting
+                this.SubmitConsoleCommand();
+            }
+        }
+
+        
+        private void SubmitConsoleCommand()
+        {
+            
+            //Just return if the console isn't up, the textbox doesn't have input focus 
+            //or there's nothing in the textbox.
+            if (!this.txtConsoleIn.Focused || string.IsNullOrEmpty(this.txtConsoleIn.Text)) return;
+
+            this._previousDebugCommands.Add(this.txtConsoleIn.Text);
+            this._debugCommandIndex = this._previousDebugCommands.Count;
+
+            var output = this._expressionEvaluator.Evaluate(this.txtConsoleIn.Text);
+
+            this.txtConsoleOut.AppendText($"\r\n{this.txtConsoleIn.Text}\r\n\t{output}");
+            this.txtConsoleIn.Clear();
+
+        }
+
+        private void ConsoleWindow_Resize(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
