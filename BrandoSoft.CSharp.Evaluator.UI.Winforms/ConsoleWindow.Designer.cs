bf947f2927937eb807e4579484fee8f55b629c0a
BrandonLegault <bdlegault@gmail.com>
2016-10-04 13:33:15 -0400

    Control beautification Add a splitter between the output and input boxes Add the splitter movement and resize logic

--- BrandoSoft.CSharp.Evaluator.UI.Winforms/ConsoleWindow.Designer.cs
+++ BrandoSoft.CSharp.Evaluator.UI.Winforms/ConsoleWindow.Designer.cs
@@ -30,30 +30,30 @@
         {
             this.txtConsoleIn = new System.Windows.Forms.TextBox();
             this.txtConsoleOut = new System.Windows.Forms.TextBox();
-            this.splContainer = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splContainer)).BeginInit();
-            this.splContainer.Panel1.SuspendLayout();
-            this.splContainer.Panel2.SuspendLayout();
-            this.splContainer.SuspendLayout();
+            this.lblSplitter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtConsoleIn
             // 
+            this.txtConsoleIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtConsoleIn.BackColor = System.Drawing.Color.Black;
-            this.txtConsoleIn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtConsoleIn.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtConsoleIn.ForeColor = System.Drawing.Color.White;
-            this.txtConsoleIn.Location = new System.Drawing.Point(0, 0);
+            this.txtConsoleIn.Location = new System.Drawing.Point(0, 98);
             this.txtConsoleIn.Margin = new System.Windows.Forms.Padding(0);
+            this.txtConsoleIn.MinimumSize = new System.Drawing.Size(4, 20);
+            this.txtConsoleIn.Multiline = true;
             this.txtConsoleIn.Name = "txtConsoleIn";
-            this.txtConsoleIn.Size = new System.Drawing.Size(855, 20);
-            this.txtConsoleIn.TabIndex = 1;
-            this.txtConsoleIn.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtConsoleIn_KeyUp);
+            this.txtConsoleIn.Size = new System.Drawing.Size(433, 20);
+            this.txtConsoleIn.TabIndex = 3;
             // 
             // txtConsoleOut
             // 
+            this.txtConsoleOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtConsoleOut.BackColor = System.Drawing.Color.Black;
-            this.txtConsoleOut.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtConsoleOut.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtConsoleOut.ForeColor = System.Drawing.Color.White;
             this.txtConsoleOut.Location = new System.Drawing.Point(0, 0);
@@ -62,52 +62,51 @@
             this.txtConsoleOut.Name = "txtConsoleOut";
             this.txtConsoleOut.ReadOnly = true;
             this.txtConsoleOut.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtConsoleOut.Size = new System.Drawing.Size(855, 643);
-            this.txtConsoleOut.TabIndex = 0;
+            this.txtConsoleOut.Size = new System.Drawing.Size(433, 96);
+            this.txtConsoleOut.TabIndex = 2;
             // 
-            // splContainer
+            // lblSplitter
             // 
-            this.splContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splContainer.Location = new System.Drawing.Point(0, 0);
-            this.splContainer.Margin = new System.Windows.Forms.Padding(0);
-            this.splContainer.Name = "splContainer";
-            this.splContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splContainer.Panel1
-            // 
-            this.splContainer.Panel1.Controls.Add(this.txtConsoleOut);
-            // 
-            // splContainer.Panel2
-            // 
-            this.splContainer.Panel2.Controls.Add(this.txtConsoleIn);
-            this.splContainer.Panel2MinSize = 0;
-            this.splContainer.Size = new System.Drawing.Size(855, 672);
-            this.splContainer.SplitterDistance = 643;
-            this.splContainer.SplitterWidth = 1;
-            this.splContainer.TabIndex = 2;
+            this.lblSplitter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSplitter.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblSplitter.Location = new System.Drawing.Point(0, 96);
+            this.lblSplitter.Name = "lblSplitter";
+            this.lblSplitter.Size = new System.Drawing.Size(433, 2);
+            this.lblSplitter.TabIndex = 4;
+            this.lblSplitter.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseDown);
+            this.lblSplitter.MouseEnter += new System.EventHandler(this.lblSplitter_MouseEnter);
+            this.lblSplitter.MouseLeave += new System.EventHandler(this.lblSplitter_MouseLeave);
+            this.lblSplitter.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseMove);
+            this.lblSplitter.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseUp);
             // 
             // ConsoleWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.splContainer);
+            this.BackColor = System.Drawing.Color.Red;
+            this.Controls.Add(this.lblSplitter);
+            this.Controls.Add(this.txtConsoleIn);
+            this.Controls.Add(this.txtConsoleOut);
+            this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(0);
+            this.MinimumSize = new System.Drawing.Size(20, 46);
             this.Name = "ConsoleWindow";
-            this.Size = new System.Drawing.Size(855, 672);
-            this.Resize += new System.EventHandler(this.ConsoleWindow_Resize);
-            this.splContainer.Panel1.ResumeLayout(false);
-            this.splContainer.Panel1.PerformLayout();
-            this.splContainer.Panel2.ResumeLayout(false);
-            this.splContainer.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splContainer)).EndInit();
-            this.splContainer.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(433, 118);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.lblSplitter_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.lblSplitter_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblSplitter_MouseUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txtConsoleOut;
+
         private System.Windows.Forms.TextBox txtConsoleIn;
-        private System.Windows.Forms.SplitContainer splContainer;
+        private System.Windows.Forms.TextBox txtConsoleOut;
+        private System.Windows.Forms.Label lblSplitter;
     }
 }
---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.consoleWindow1 = new BrandoSoft.CSharp.Evaluator.UI.Winforms.ConsoleWindow();
+            this.SuspendLayout();
+            // 
+            // consoleWindow1
+            // 
+            this.consoleWindow1.AcceptKey = System.Windows.Forms.Keys.Return;
+            this.consoleWindow1.BackColor = System.Drawing.Color.Red;
+            this.consoleWindow1.Location = new System.Drawing.Point(100, 127);
+            this.consoleWindow1.Margin = new System.Windows.Forms.Padding(0);
+            this.consoleWindow1.MinimumSize = new System.Drawing.Size(20, 46);
+            this.consoleWindow1.MultilineInput = false;
+            this.consoleWindow1.Name = "consoleWindow1";
+            this.consoleWindow1.OutputText = "";
+            this.consoleWindow1.Size = new System.Drawing.Size(433, 199);
+            this.consoleWindow1.SplitterDistance = 179;
+            this.consoleWindow1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 386);
+            this.Controls.Add(this.consoleWindow1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrandoSoft.CSharp.Evaluator.UI.Winforms.ConsoleWindow consoleWindow1;
+    }
+}
+
